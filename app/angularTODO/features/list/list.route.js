80b12f613e300fcc11984913e9a3b6f734be67df
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-03-06 22:43:47 -0500

    added list.controller

--- app/angularTODO/features/list/list.route.js
+++ app/angularTODO/features/list/list.route.js
@@ -6,7 +6,7 @@ function listConfig($stateProvider) {
 
 	$stateProvider.state({
 		name: 'list',
-		url: '/list',
+		url: '/',
 		templateUrl: '/angularTODO/features/list/list.html',
 		controller: 'ListCtrl',
 		controllerAs: 'ListVM'
